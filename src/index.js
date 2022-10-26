847ed7991c4d552d1c2f83adecf33ac209e63420
Latisha Copeland <lcopeland001@gmail.com>
2022-10-26 12:15:24 -0500

    property reducer

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import store from './redux/store';
-
+import logger from 'redux-logger';
 import App from './components/App/App';
+
+// Reducers
+const propety = (state = '', action) => {
+  if (action.type === 'SET_PROPERTY') {
+      return action.payload;
+  } else if (action.type === 'CLEAR_ALL') {
+      return '';
+  }
+  return state;
+}
+
+// Redux store! Keeps track of all reducers
+const storeInstance = createStore(
+  combineReducers(
+      {
+          name,
+          
+      }
+  ),
+  applyMiddleware(logger)
+);
 
 ReactDOM.render(
   <Provider store={store}>
